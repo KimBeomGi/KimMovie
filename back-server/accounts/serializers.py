4abc16b1881bccbcc346ab8fa4bb8efdb07f78ad
김범기 <dyd1308@naver.com>
2023-05-22 11:30:02 +0900

    230522 follow이름추가

--- back-server/accounts/serializers.py
+++ back-server/accounts/serializers.py
@@ -4,6 +4,8 @@ from .models import User
 class FollowSerializer(serializers.ModelSerializer):
     followings_count = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
+    followings_name = serializers.SerializerMethodField()
+    followers_name = serializers.SerializerMethodField()
     
     def get_followings_count(self, obj):
         return obj.followings.count()
@@ -11,9 +13,16 @@ class FollowSerializer(serializers.ModelSerializer):
     def get_followers_count(self, obj):
         return obj.followers.count()
     
+    def get_followings_name(self, obj):
+        return [user.username for user in obj.followings.all()]
+
+    def get_followers_name(self, obj):
+        return [user.username for user in obj.followers.all()]
+    
     class Meta:
         model = User
-        fields = ('followings_count', 'followers_count', 'followings', 'followers')
+        # fields = ('followings_count', 'followers_count', 'followings', 'followers')
+        fields = ('followings_count', 'followers_count', 'followings', 'followers', 'followings_name', 'followers_name')
         
         
 # class CustomUserSerializer(serializers.ModelSerializer):
@@ -25,6 +34,8 @@ class FollowSerializer(serializers.ModelSerializer):
 class CustomUserSerializer(serializers.ModelSerializer):
     followings_count = serializers.SerializerMethodField()
     followers_count = serializers.SerializerMethodField()
+    followings_name = serializers.SerializerMethodField()
+    followers_name = serializers.SerializerMethodField()
     
     def get_followings_count(self, obj):
         return obj.followings.count()
@@ -32,11 +43,18 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def get_followers_count(self, obj):
         return obj.followers.count()
     
+    def get_followings_name(self, obj):
+        return [user.username for user in obj.followings.all()]
+
+    def get_followers_name(self, obj):
+        return [user.username for user in obj.followers.all()]
+    
     class Meta:
         model = User
         fields = ('id', 'password', 'last_login', 'is_superuser', 
                   'username', 'first_name', 'last_name', 'email', 
                   'is_staff', 'is_active', 'date_joined', 'exp', 'point',
                   'groups', 'user_permissions', 'followings', 'followers',
+                  'followings_name', 'followers_name',
                   'followings_count', 'followers_count', 'idealmovie',)     # 'idealmovie', 추가함
         
--- back-server/accounts/urls.py
+++ back-server/accounts/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     # follow 주소
     # path('accounts/api/v1/', include('accounts.urls'))
-    path('follow/<int:user_pk>/', views.follow),
-    path('unregister/', views.unregister),
-    path('profile/<int:user_pk>/', views.profile),
+    path('follow/<int:user_pk>/', views.follow),            # follow 하기
+    path('unregister/', views.unregister),                  # 회원 탈퇴
+    path('profile/<int:user_pk>/', views.profile),          # profile 보기
     
 ]
