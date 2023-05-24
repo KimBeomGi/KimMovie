3d0fd2f44819cc71356ee55f4dc5ead413ab2f01
KimHyeongJin <gudwls9966@naver.com>
2023-05-24 15:58:02 +0900

    메인화면영상

--- front-server/src/components/HeaderMovie.vue
+++ front-server/src/components/HeaderMovie.vue
@@ -1,80 +1,197 @@
 <template>
-  <div>
-    <div
-      id="Live"
-      class="player"
-      data-property="{
-          videoURL: 'https://www.youtube.com/watch?v=XyHr-s3MfCQ',
-          mute: false,
-          showControls: false,
-          useOnMobile: true,
-          quality: 'hd2160',
-          containment: 'self',
-          loop: true,
-          autoPlay: true,
-          stopMovieOnBlur: true, 
-          startAt: 0,
-          opacity: 1
-        }"
-      style="width: 1920px; height: 1080px"
-    ></div>
-    <div class="description" style="text-align: start;">
-      <h1 style="font-size: 100px;">가디언즈 오브 갤럭시</h1>
-      <h1>‘가모라’를 잃고 슬픔에 빠져 있던 ‘피터 퀼’이 </h1>
-      <h1>위기에 처한 은하계와 동료를 지키기 위해 다시 한번 가디언즈 팀과 힘을 모으고,
-</h1>
-      <h1>성공하지 못할 경우 그들의 마지막이 될지도 모르는 미션에 나서는 이야기</h1>
+  <div style="background-color: black; color: #ffffff;">
+    <div>
+      <iframe
+  id="inlineFrameExample"
+  :src="`${moviekey}?autoplay=1`"
+  width="1920"
+  height="1080"
+></iframe>
 
     </div>
-    </div>
+    <!-- 게시글을 생성하는 뷰인 CreateView로 이동하는 링크 생성-->
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name : 'HeaderMovie',
-    data(){
-      return{
-        articles : this.$store.state.articles
+  name: 'MovieDetailView',
+  data() {
+    return {
+      moviedetail: null,
+      moviekey: '',
+      showFullOverview: false,
+      maxOverviewLength: 200,
+      poster : '',
+      like : '',
+      like_users_num : ''
+    };
+  },
+  computed: {
+    likebt(){
+      if (this.like){
+        return '취소'
+      }else{
+        return '좋아요'
       }
     },
-    created() {
+    truncatedOverview() {
+      return this.showFullOverview
+        ? this.moviedetail?.overview
+        : this.moviedetail?.overview.slice(0, this.maxOverviewLength) + '...';
     },
-    methods : {
-
+    isOverviewTruncated() {
+      return this.moviedetail?.overview.length > this.maxOverviewLength;
     },
-    mounted() {
-    // eslint-disable-next-line
-    jQuery(function () {
-      // eslint-disable-next-line
-      jQuery("#Live").YTPlayer();
-      
-    });
-    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    movieId(){
+      return this.moviedetail?.id
+    },
+    movieTitle(){
+      return this.moviedetail?.title
+    }
   },
-}
+  created() {
+    this.getMovie();
+    this.scrollToTop()
+
+  },
+  methods: {
+    getMovie() {
+      axios({
+        method: 'get',
+        url: `http://localhost:8000/api/v1/recommend/`,
+        // headers: this.$store.getters.authHeader,
+      })
+        .then((res) => {
+          // console.log(res.data[0].key)
+          this.moviedetail = res.data;
+          this.moviekey = `https://www.youtube.com/embed/${res.data[0].key}`;
+         
+        })
+        .catch((err) => {
+          // alert('에러')
+          alert('실패');
+          console.log(err);
+        });
+    },
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+    toggleFavorite(){
+      axios({
+        method: 'post',
+        url: `http://localhost:8000/api/v1/like/${this.moviedetail?.id}/`,
+        headers: this.$store.getters.authHeader,
+      })
+        .then((res) => {
+          // this.$router.push({name:'CommunityView'})
+          // this.$router.go(-1)
+          this.like_users_num = res.data.like_users_num
+          this.like = res.data.is_liked
+          // console.log(res.data.is_liked)
+          console.log(res.data)
+        })
+        .catch((err) => {
+          console.log(this.moviedetail?.id)
+          console.log(err)
+        })
+    }
+  },
+};
 </script>
 
-<style>
-#Live {
-  z-index: -5;
+<style scoped>
+.overview-truncated {
+  margin-left: 200px;
+  margin-right: 200px;
 }
-.inline-YTPlayer {
-  width: 1920px ;
-  height: 1080px ;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, 0%);
-}
-
-.YTPOverlay {
-  width: 1920px !important;
-  height: 1080px !important;
+.container {
+  max-width: 2000px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  align-items: center;
 }
 
-.description {
-  position: absolute;
-  top: 700px;
-  left: 100px;
-  color: white;
+iframe {
+  width: 100%;
+  height: 600px;
+}
+
+button {
+  margin-top: 10px;
+}
+
+.poster {
+  flex: 1;
+  text-align: center;
+  position: sticky;
+  top: 20px;
+}
+
+.poster-image {
+  width: 200px;
+}
+
+.details {
+  flex: 1;
+  margin-left: 20px;
+}
+
+.back-link {
+  color: #ffffff;
+  text-decoration: none;
+  margin-top: 20px;
+  display: inline-block;
+  border: 1px solid #ffffff;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.action-buttons {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.create-link {
+  color: #ffffff;
+  text-decoration: none;
+  background-color: #4caf50;
+  padding: 10px 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.create-link:hover {
+  background-color: #45a049;
+}
+
+.like-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #007bff;
+  color: #ffffff;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 20px;
+  width: 100px;
+}
+
+.like-button:hover {
+  background-color: #0056b3;
+}
+
+.like-count {
+  margin-left: 20px;
+  margin-top: 15px;
+  font-size: 30px;
+  font-weight: bold;
+  color:white
 }
 </style>
